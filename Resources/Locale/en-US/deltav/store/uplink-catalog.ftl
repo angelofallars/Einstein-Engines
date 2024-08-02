DeltaV-Station/Delta-v

Split Emag in half (#1426)
* make changes

* fix yaml linter bug

* locales

* consolidations

* fix DeltaV comment appearing in uplink

* e

* Update emag.yml

Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

* emag invalid target locale

* change id 'airlockaccessoverride' to 'doorjack'

* name consolidations + doorjack sprite

* Update Resources/Locale/en-US/deltav/store/uplink-catalog.ftl

Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

* Update Resources/Locale/en-US/store/uplink-catalog.ftl

Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

* Update Resources/Locale/en-US/emag/emag.ftl

Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

* Update Resources/Prototypes/DeltaV/Catalog/uplink_catalog.yml

Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

* Update Resources/Prototypes/DeltaV/Entities/Objects/Tools/emag.yml

Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

* deltanedas

---------

Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>
Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

Resources/Locale/en-US/deltav/store/uplink-catalog.ftl
@@ -13,3 +13,6 @@ uplink-syndicate-radio-implanter-bundle-desc = Two implanters for the price of o
 
 uplink-generic-radio-implanter-name = Generic Radio Implanter
 uplink-generic-radio-implanter-desc = A cranial implant with a bluespace compartment for a single encryption key (not included). Put in a key of your choice, and you can talk using it like you would with any headset.
+
+uplink-doorjack-name = Airlock Access Override
+uplink-doorjack-desc = A specialized cryptographic sequencer, designed solely to doorjack NanoTrasen's updated airlocks. Does not tamper with anything else.

angelofallars/Einstein-Engines

Resources/Locale/en-US/deltav/store/uplink-catalog.ftl
@@ -4,3 +4,6 @@ uplink-reinforcement-radio-nukie-mouse-desc = Calls in a specially trained mouse
 # Implants
 uplink-bionic-syrinx-implanter-name = Bionic Syrinx Implanter
 uplink-bionic-syrinx-implanter-desc = An implant that enhances a harpy's natural talent for mimicry to let you adjust your voice to whoever you can think of.
+
+uplink-doorjack-name = Airlock Access Override
+rplink-doorjack-desc = A specialized cryptographic sequencer, designed solely to doorjack NanoTrasen's updated airlocks. Does not tamper with anything else.